5450694108697efc0d08fcb2223cf6154d8d0a37
Antonella Aiko <aikoescolaa@gmail.com>
2025-03-18 16:09:43 -0300

    Adicionando Exercício "Café"

---
+++ café/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<title> Café </Title>
+	</head>
+	<body>
+	<fieldset>
+		<legend> Café da Hello Kitty!! </legend>
+		<form method="post" action = "calcular.php">
+
+        Informe seu nome: 
+        <input type = "text" name = "$nome" size = "25" />
+        <br><br>
+
+		1 – Bolo da Hello Kitty (R$15,00) <br>
+        2 – Donut do Pompompurin (R$10,00) <br>
+        3 – Cupcake da My Melody (R$12,00) <br>
+        4 – Sanduíche do Chococat (R$12,00) <br>
+        5 – Mini Quiche da Kuromi (R$14,00) <br>
+        <br><br>
+
+        Qual opção do menu você deseja? 
+        <input type = "text" name = "$l" size = "25" />
+        <br><br>
+
+        Quantas da opção escolhida você deseja comprar?
+        <input type = "text" name = "$nl" size = "25" />
+        <br><br>
+
+        1 – Hello Kitty Latte (R$8,00) <br>
+        2 – My Melody Chocolate (R$10,00) <br>
+        3 – Kuromi Espresso (R$7,00) <br>
+        4 – Cinnamoroll Milkshake (R$12,00) <br>
+        5 – Pompompurin Caramelo (R$10,00) <br>
+        6 – Chá do Chococat (R$8,00) <br>
+        7 – Keroppi Green Tea (R$9,00) <br>
+        <br><br>
+
+        Qual opção de bebida você deseja?    
+        <input type = "text" name = "$b" size = "25" />
+        <br><br>
+
+        Quantas bebidas você deseja comprar?
+        <input type = "text" name = "$nb" size = "25" />
+        <br><br>
+		
+		<input type = "submit" name = "botão" value = "Valor Total" />
+		
+		</form>
+	</fieldset>
+	</body>
+	</html>
